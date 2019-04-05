4fe7ec132350ad280d5d870c232e1d12151c2599
DanWBR <danielwag@gmail.com>
2019-04-05 11:23:54 -0400

    missing interface implementation

--- Scintilla.Eto.Shared/ScintillaControl.cs
+++ Scintilla.Eto.Shared/ScintillaControl.cs
@@ -24,7 +24,49 @@ namespace Eto.Forms.Controls.Scintilla.Shared
             void ResetDefaultStyle();
 
             void ClearAllStyles();
-            
+
+        }
+
+        public string ScriptText
+        {
+            get
+            {
+                return Handler.ScriptText;
+            }
+            set
+            {
+                Handler.ScriptText = value;
+            }
+        }
+
+        public void SetKeywords(int level, string keywords)
+        {
+            Handler.SetKeywords(level, keywords);
+        }
+
+        public void SetStyle(int styleID, int item, object value)
+        {
+            Handler.SetStyle(styleID, item, value);
+        }
+
+        public void SetFont(string fontname)
+        {
+            Handler.SetFont(fontname);
+        }
+
+        public void SetFontSize(int fontsize)
+        {
+            Handler.SetFontSize(fontsize);
+        }
+
+        public void ResetDefaultStyle()
+        {
+            Handler.ResetDefaultStyle();
+        }
+
+        public void ClearAllStyles()
+        {
+            Handler.ClearAllStyles();
         }
 
     }
